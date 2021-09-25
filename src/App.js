f9c502eef9c793ea465f71ca01ae8466bcb2fb3e
Gokul-pc <gokulpc855@gmail.com>
2021-09-25 14:09:36 +0530

    netflix sample added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import "./App.css"
+import Banner from './components/Banner/Banner'
+import Rowpost from './components/Rowpost/Rowpost'
+import { actionMovies, animation, fantasy, horror,  originals } from './components/constants/constants'
+
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar/>
+      <Banner/>
+      <Rowpost url={originals} title="Netflix Originals" />
+      <Rowpost url={actionMovies} title="Action Movies"  isSmall />  
+      <Rowpost url={horror} title="Horror Movies"  isSmall /> 
+      <Rowpost url={animation} title="Animation Movies"  isSmall /> 
+      <Rowpost url={fantasy} title="Fantasy Movies"  isSmall /> 
+   
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/constants/constants.js
@@ -0,0 +1,10 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY='0c6f79dad27753abc02d92b96a5ab148';
+export const imageUrl='https://image.tmdb.org/t/p/original';
+export const originals=`discover/tv?api_key=${API_KEY}&with_networks=213`
+export const actionMovies=`discover/movie?api_key=${API_KEY}&with_genres=28`
+export const horror=`discover/movie?api_key=${API_KEY}&with_genres=27`
+export const animation=`discover/movie?api_key=${API_KEY}&with_genres=16`
+export const fantasy=`discover/movie?api_key=${API_KEY}&with_genres=14`
+
+
